5427088ebe791aa2ce755f7abd323441e636b0f8
Dylan <mosuan@gmail.com>
2019-03-14 23:00:20 +1100

    Added basic UI elements

---
+++ TapGame/TapGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapGame
+//
+//  Created by Dylan on 14/3/19.
+//  Copyright © 2019 Dylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
